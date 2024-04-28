82aef29d81cb4c6a86b24cdf1a80af7e036a5d72
Georgios Kazantzidis <george_kazantzidis@outlook.com>
2024-04-28 09:03:17 +0200

    update

--- Places.R
+++ Places.R
@@ -1,97 +1,7 @@
 library("tidyverse")
 library("leaflet")
 
-Places <- readxl::read_excel("Places.xlsx", col_types = c("text","text", "text",
-                                                          "numeric", "numeric", 
-                                                          "numeric", "date", "text")) %>% 
-  mutate("Country" = factor(Country) ) %>% 
-  mutate("Info" = paste("Place:", Place, "<br/>", 
-                        "Date:", Date, "<br/>", 
-                        "Year:", Year, "<br/>",
-                        "People:", People, "<br/>",
-                        "Memory:", Memory))
 
-
-quakeIcons <- iconList(Switzerland = makeIcon("icons/Swiss.jfif", iconWidth = 24, iconHeight = 24),
-                       Netherlands = makeIcon("icons/Neth.jfif", iconWidth = 24, iconHeight = 24),
-                       Belgium = makeIcon("icons/Be.jfif", iconWidth = 24, iconHeight = 24),
-                       Bulgaria = makeIcon("icons/Bu.jfif", iconWidth = 24, iconHeight = 24),
-                       Italy = makeIcon("icons/It.jfif", iconWidth = 24, iconHeight = 24),
-                       Scotland = makeIcon("icons/Sc.jfif", iconWidth = 24, iconHeight = 24),
-                       Kosovo = makeIcon("icons/Ko.jfif", iconWidth = 24, iconHeight = 24),
-                       Greece = makeIcon("icons/gr.jfif", iconWidth = 24, iconHeight = 24),
-                       France = makeIcon("icons/Fra.jfif", iconWidth = 24, iconHeight = 24),
-                       Irland = makeIcon("icons/Irl.jfif", iconWidth = 24, iconHeight = 24),
-                       Spain = makeIcon("icons/Sp.jfif", iconWidth = 24, iconHeight = 24),
-                       Norway = makeIcon("icons/No.jfif", iconWidth = 24, iconHeight = 24),
-                       Denmark = makeIcon("icons/Den.jfif", iconWidth = 24, iconHeight = 24),
-                       Germany = makeIcon("icons/Ge.jfif", iconWidth = 24, iconHeight = 24),
-                       Cyprus = makeIcon("icons/cy.jfif", iconWidth = 24, iconHeight = 24))
-
-
-
-  leaflet() %>%
-  addProviderTiles(providers$CartoDB.Positron) %>% 
-  addMarkers(data = Places,
-             lat = ~Latitude,
-             lng = ~Longitude,
-             label = Places$Place, 
-             clusterOptions = markerClusterOptions(spiderLegPolylineOptions = list(weight = .5),
-                                                   freezeAtZoom = "maxKeepSpiderify"),
-             popup = Places$Info,
-             icon = ~quakeIcons[Country],
-             options = markerOptions(opacity = .6)) 
-#### People count ####
-  People_count <- Places %>% 
-    filter(Year %in% c(2022, 2023, 2024)) %>%
-    select(People) %>% 
-    separate_rows(People, sep = ", ") %>%
-    drop_na() %>% 
-    mutate(People = factor(People)) %>%
-    count(People) %>% 
-    arrange(n) 
-  
-  
-  People_count %>% 
-    filter(n>4) %>% 
-    ggplot(aes(y = n, x = reorder(People, -n) ))+
-    geom_bar(position="dodge", stat="identity", col = "lightgreen", fill = "lightblue", alpha = .8)+
-    ylab(label = "Number of places")+
-    xlab(label = "Peple")+
-    theme_minimal()
-  
-  Places %>% 
-    filter(Year > 2015) %>% 
-    separate_rows(People, sep = ", ") %>% 
-    filter(People == "Sofia") %>% 
-    select(Place, Date, Memory)
-  
-#### 2023 places ####
-
- Places %>% 
-    filter(Year == "2023") %>% view()
-  
-  
-#### Sofia and me map ####
-  
-  Sofia_Places <- Places %>% 
-    filter(str_detect(People, "Philipp"))
-    filter(Year == "2023")
-  
-  leaflet() %>%
-    addProviderTiles(providers$CartoDB.Positron) %>% 
-    addMarkers(data = Sofia_Places,
-               lat = ~Latitude,
-               lng = ~Longitude,
-               label = Sofia_Places$Place, 
-               clusterOptions = markerClusterOptions(spiderLegPolylineOptions = list(weight = .5),
-                                                     freezeAtZoom = "maxKeepSpiderify"),
-               popup = Sofia_Places$Info,
-               icon = ~quakeIcons[Country],
-               options = markerOptions(opacity = .6)) 
-  
-  
-########################
   
   Places <- readxl::read_excel("Places_V2.xlsx", col_types = c("text","text", "text", "text", "text",
                                                             "numeric", "numeric", 
@@ -136,7 +46,8 @@ quakeIcons <- iconList(Switzerland = makeIcon("icons/Swiss.jfif", iconWidth = 24
                          Norway = makeIcon("icons/No.jfif", iconWidth = 24, iconHeight = 24),
                          Denmark = makeIcon("icons/Den.jfif", iconWidth = 24, iconHeight = 24),
                          Germany = makeIcon("icons/Ge.jfif", iconWidth = 24, iconHeight = 24),
-                         Cyprus = makeIcon("icons/cy.jfif", iconWidth = 24, iconHeight = 24))
+                         Cyprus = makeIcon("icons/cy.jfif", iconWidth = 24, iconHeight = 24),
+                         Canada = makeIcon("icons/Ca.jfif", iconWidth = 24, iconHeight = 24))
   
   
   # All places with zoom grouping 
@@ -170,5 +81,58 @@ quakeIcons <- iconList(Switzerland = makeIcon("icons/Swiss.jfif", iconWidth = 24
       overlayGroups = unique(Places$Trip_name),
       options = layersControlOptions(collapsed = FALSE)
     ) 
+  
+  
+  #### People count ####
+  People_count <- Places %>% 
+    filter(Year %in% c(2022, 2023, 2024)) %>%
+    select(People) %>% 
+    separate_rows(People, sep = ", ") %>%
+    drop_na() %>% 
+    mutate(People = factor(People)) %>%
+    count(People) %>% 
+    arrange(n) 
+  
+  
+  People_count %>% 
+    filter(n>4) %>% 
+    ggplot(aes(y = n, x = reorder(People, -n) ))+
+    geom_bar(position="dodge", stat="identity", col = "lightgreen", fill = "lightblue", alpha = .8)+
+    ylab(label = "Number of places")+
+    xlab(label = "Peple")+
+    theme_minimal()
+  
+  Places %>% 
+    filter(Year > 2015) %>% 
+    separate_rows(People, sep = ", ") %>% 
+    filter(People == "Sofia") %>% 
+    select(Place, Date, Memory)
+  
+  #### 2023 places ####
+  
+  Places %>% 
+    filter(Year == "2023") %>% view()
+  
+  
+  #### Sofia and me map ####
+  
+  Sofia_Places <- Places %>% 
+    filter(str_detect(People, "Nina"))
+  filter(Year == "2023")
+  
+  leaflet() %>%
+    addProviderTiles(providers$CartoDB.Positron) %>% 
+    addMarkers(data = Sofia_Places,
+               lat = ~Latitude,
+               lng = ~Longitude,
+               label = Sofia_Places$Place, 
+               clusterOptions = markerClusterOptions(spiderLegPolylineOptions = list(weight = .5),
+                                                     freezeAtZoom = "maxKeepSpiderify"),
+               popup = Sofia_Places$Info,
+               icon = ~quakeIcons[Country],
+               options = markerOptions(opacity = .6)) 
+  
+  
+  ########################
   
   
